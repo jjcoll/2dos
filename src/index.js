67e37c3bb8df8dc4d7a74996c89486c547f4d0d1
Jordi Coll <jjordicoll@gmail.com>
2022-06-16 18:55:33 +0200

    added hover effect to filters

--- src/index.js
+++ src/index.js
@@ -11,12 +11,14 @@ themeBtn.addEventListener('click', () => {
     document.documentElement.style.setProperty('--background', '#1A1A1A');
     document.documentElement.style.setProperty('--border', '#4D4D4D');
     document.documentElement.style.setProperty('--text', '#FFFFFF');
+    document.documentElement.style.setProperty('--background-hover', '#313131');
   } else {
     themeBtn.setAttribute("name", "sunny-outline")
     theme = "d"
     document.documentElement.style.setProperty('--background', '#FFFFFF');
     document.documentElement.style.setProperty('--border', '#F0F0F0');
     document.documentElement.style.setProperty('--text', '#000000');
+    document.documentElement.style.setProperty('--background-hover', '#e6e6e6');
   }
 })
 

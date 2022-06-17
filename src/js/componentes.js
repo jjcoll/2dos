a26e1696c0cb3a06159e884b73806b5c81e9ced5
Jordi Coll <jjordicoll@gmail.com>
2022-06-17 16:58:51 +0200

    remove todos from list

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -7,16 +7,16 @@ const todoInput = document.querySelector('.add-todo__input')
 
 // crear en HTML todo
 export const crearTodoHtml = (todo) => {
-  let html = ` 
-          <p class="task-name ${ (todo.completed) ? "completed" : "" }" data-id="${todo.id}">${todo.task}</p>
+  let html = `<div class="todo" data-id=${todo.id}> 
+          <p class="task-name">${todo.task}</p>
           <ion-icon class="close-icon" name="close-outline"></ion-icon>
+          </div>
         `
 
   const div = document.createElement('div')
-  div.classList.add('todo')
   div.innerHTML = html;
 
-  todoListEl.append( div )
+  todoListEl.append( div.firstChild )
 
   return html
 }
@@ -34,16 +34,17 @@ todoInput.addEventListener('keyup', (ev) => {
 
 // Manejar clicks en todo list
 todoListEl.addEventListener('click', (ev) => {
-
-  // Toggle completed and not completed
-  if (ev.target.classList.contains("task-name")) {
-    const todoEl = ev.target
-    const todoId = todoEl.getAttribute("data-id")
-   
-    todoEl.classList.toggle("completed")
-    todoList.toggleTodo(todoId) 
-  } else {
-    console.log("clicked on another place")
+  
+  const todoElement = ev.target.parentElement
+  const todoName = ev.target.localName
+  const todoId = todoElement.getAttribute("data-id")
+ 
+  if (todoName === "p") {
+    ev.target.classList.toggle("completed")
+    todoList.toggleTodo( todoId )
+  } else if(todoName === "ion-icon") {
+    todoElement.remove()
+    todoList.removeTodo( todoId )
   }
 })
 
